1af85d8ab1688ff9caaadc78c92e43edec474e3d
emmalynnn97 <emmalynnn97@gmail.com>
2020-09-20 11:27:11 -0700

    made the dots functional, they dynamically generate based on the number of slides, and the active slide now corresponds to the active dot

--- slider/script.js
+++ slider/script.js
@@ -2,10 +2,8 @@ const sliderContainer = document.querySelector('.slider-container');
 const sliderOverlay = document.querySelector('.slider-overlay');
 const sliderBtnRight = document.querySelector('.slider-btn-right');
 const sliderBtnLeft = document.querySelector('.slider-btn-left');
-const sliders = [
-    {
-        image: "./slider-bg-1.jpg"
-    },
+const sliderDotContainer = document.querySelector('.slider-dot-container')
+const slides = [
     {
         image:'./slider-bg-2.jpg'
     },
@@ -16,20 +14,35 @@ const sliders = [
         image:'./slider-bg-4.jpg'
     }
 ]
-
+for(var k = 0; k < slides.length;k++){
+    var newDot = document.createElement('div');
+    if(k == 0){
+        newDot.classList.add('slider-dot-active');
+    }
+    else{
+        newDot.classList.add('slider-dot');
+    }
+    sliderDotContainer.appendChild(newDot);
+}
 //Set default image as first one in array
 var i = 0;
-sliderContainer.style.backgroundImage=`url("${sliders[0].image}")`
+sliderContainer.style.backgroundImage=`url("${slides[0].image}")`
 
-
-
-sliderBtnRight.addEventListener('click',()=>{
-    i = (i + 1) % sliders.length;
-    sliderContainer.style.backgroundImage=`url("${sliders[i].image}")` 
-})
+//Have left button wait until click, on click cycle the slide backwards by 1 space
 sliderBtnLeft.addEventListener('click',()=>{
+    //Only cycle the slide backwards by 1 space if i is positive
     if(i > 0){
-        i = (i - 1) % sliders.length;
-        sliderContainer.style.backgroundImage=`url("${sliders[i].image}")`
+        sliderDotContainer.children[i].style.backgroundColor='gray'
+        i = (i - 1) % slides.length;
+        sliderDotContainer.children[i].style.backgroundColor='white'
+        sliderContainer.style.backgroundImage=`url("${slides[i].image}")`
     }
+})
+
+//Have right button wait until click, on click cycle the slide forwards by 1 space
+sliderBtnRight.addEventListener('click',()=>{
+    sliderDotContainer.children[i].style.backgroundColor='gray'
+    i = (i + 1) % slides.length;
+    sliderDotContainer.children[i].style.backgroundColor='white'
+    sliderContainer.style.backgroundImage=`url("${slides[i].image}")` 
 })
